b99ce2dd2aff7f34d2fd36e5c18321ab268bbd49
Stanvic <stanvicbest@gmail.com>
2024-06-13 15:12:12 -0700

    comprehensive year adjusted

--- comprehensive.php
+++ comprehensive.php
@@ -1,6 +1,6 @@
 <?php require 'inc/header.php'; ?>
 <?php if(isset($_SESSION['username'])):
-$year = date('Y');
+$year = '2023';
 $sql = "SELECT * FROM participants WHERE year = '$year'";
 $query = mysqli_query($conn, $sql);
 $info = mysqli_num_rows($query);
