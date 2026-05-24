60aefc0d42209f27b40138ce8d6934034167094f
nneliifeanyi <stanvicbest@gmail.com>
2026-05-24 13:33:40 +0100

    db connect

--- app/config/config.php
+++ app/config/config.php
@@ -2,13 +2,27 @@
 date_default_timezone_set("Africa/Lagos");
 // DB Params
 define("DB_HOST", "localhost");
-define("DB_USER", "stanvicc_fishersTeam");
-define("DB_PASS", "fishersTeam2023");
-define("DB_NAME", "stanvicc_sulejadisciples");
+define("DB_USER", "stanvicc_SulejaDisciplesAdmin");
+define("DB_PASS", "Avalanche@25");
+define("DB_NAME", "stanvicc_suleja-discipleship-labour");
 
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
 define('URLROOT', 'https://sulejadisciples.stanvic.com.ng');
 // Site Name
-define('SITENAME', 'Greater Bethesda');
+define('SITENAME', 'Suleja Disciples');
+
+
+
+// define("DB_HOST", "localhost");
+// define("DB_USER", "root");
+// define("DB_PASS", "");
+// define("DB_NAME", "sd");
+
+// // App Root
+// define('APPROOT', dirname(dirname(__FILE__)));
+// // URL Root
+// define('URLROOT', 'http://localhost/sulejadisciples');
+// // Site Name
+// define('SITENAME', 'Suleja Disciples');
